fb8f941cb0617e01e096aa619135025574052c73
Vladimir <Vladimir@Vladimir-MSI>
2018-06-26 01:47:18 +0800

    I changed out message.

--- Git-test-project/src/by/vovanizovtsev/test/Main.java
+++ Git-test-project/src/by/vovanizovtsev/test/Main.java
@@ -3,7 +3,7 @@ package by.vovanizovtsev.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!!");
 	}
 
 }
